d8389f941f148c42f6bc223e862c6623f6c9c73b
Charlie Birks <charlie@daft.games>
2023-07-04 13:57:48 +0100

    Add a basic scrolling console

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <seven/prelude.h>
 #include <seven/hw/bios/decompression.h>
 #include <seven/hw/video.h>
@@ -5,6 +9,8 @@
 #include <seven/hw/video/bg_tiled.h>
 
 #include "assets/8x8font.h"
+
+static int cursor_x = 0, cursor_y = 0;
 
 static void load_font() {
     BG_PALETTE[0] = 0;
@@ -24,13 +30,56 @@ static void clear_text() {
     biosCpuSet(&zero, MAP_BASE_ADDR(2), CS_SRC_FIXED | (0x800 / 4));
 }
 
-static void write_text(int x, int y, const char *text) {
+static void write_text_pos(int x, int y, const char *text) {
     // char block 2
-    uint16_t *ptr = MAP_BASE_ADDR(2) + x + y * 32;
+    uint16_t *ptr = MAP_BASE_ADDR(2) + x * 2 + y * 64;
 
     for(; *text; text++)
         *ptr++ = *text - ' ';
 }
+
+static void write_text(const char *text) {
+    for(; *text; text++) {
+        if(*text == '\n') {
+            cursor_y++;
+            cursor_x = 0;
+
+            if(cursor_y == 20) {
+                // scroll
+                // using actual scrolling would be more efficient, but it really doesn't matter here
+                for(int y = 0; y < 20; y++)
+                    biosCpuSet(MAP_BASE_ADDR(2) + (y + 1) * 64, MAP_BASE_ADDR(2) + y * 64, 64 / 4);
+                cursor_y--;
+            }
+        } else if(*text == '\r')
+            cursor_x = 0;
+        else {
+            uint16_t *ptr = MAP_BASE_ADDR(2) + cursor_x * 2 + cursor_y * 64;
+            *ptr = *text - ' ';
+            cursor_x++;
+        }
+    }
+}
+
+static int log_printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    // get length
+    va_list tmp_args;
+    va_copy(tmp_args, args);
+    int len = vsnprintf(NULL, 0, format, tmp_args) + 1;
+    va_end(tmp_args);
+
+    char *buf = malloc(len);
+    int ret = vsnprintf(buf, len, format, args);
+    va_end(args);
+
+    write_text(buf);
+
+    free(buf);
+    return ret;
+  }
 
 int main() {
     REG_DISPCNT = VIDEO_MODE_REGULAR | VIDEO_BG0_ENABLE;
@@ -47,7 +96,7 @@ int main() {
     load_font();
     clear_text();
 
-    write_text(0, 0, "Hello!");
+    write_text("Hello!\n\n");
 
     while(true) {
         biosVBlankIntrWait();
