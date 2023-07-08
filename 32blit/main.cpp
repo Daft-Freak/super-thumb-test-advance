28b1533130654caa5b407cd3fe7140fc92d9732e
Charlie Birks <charlie@daft.games>
2023-07-08 22:25:10 +0100

    Add some feedback to 32blit port
    The bare minimum. Screen goes red if something failed

--- 32blit/main.cpp
+++ 32blit/main.cpp
@@ -4,13 +4,15 @@
 
 #include "tests/tests.h"
 
+static bool failed = false;
+
 static void new_test_group(const char *label) {
     blit::debugf("%s ", label);
 }
 static void test_failed(int i, uint32_t val, uint32_t expected) {
+    failed = true;
     blit::debugf("\n%i: %x != %x\n", i, val, expected);
 }
-
 
 void init() {
     blit::debugf("Starting...\n");
@@ -20,7 +22,9 @@ void init() {
 }
 
 void render(uint32_t) {
-
+    // TODO: improve results display
+    blit::screen.pen = failed ? blit::Pen{255, 0, 0} : blit::Pen{0, 255, 0};
+    blit::screen.clear();
 }
 
 void update(uint32_t) {
