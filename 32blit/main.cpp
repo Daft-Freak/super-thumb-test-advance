da08c951c7e2d475fd3fb1c8dcb165e30e54df7a
Charlie Birks <charlie@daft.games>
2024-09-08 17:22:47 +0100

    Improve 32blit test display a bit
    It now displays which group(s) failed

--- 32blit/main.cpp
+++ 32blit/main.cpp
@@ -6,11 +6,17 @@
 
 static bool failed = false;
 
+static std::vector<std::tuple<std::string_view, bool>> test_groups; // name, failed
+
 static void new_test_group(const char *label) {
     blit::debugf("%s ", label);
+
+    test_groups.push_back({label, false});
 }
 static void test_failed(int i, uint32_t val, uint32_t expected) {
     failed = true;
+    std::get<1>(test_groups.back()) = true;
+
     blit::debugf("\n%i: %x != %x\n", i, val, expected);
 }
 
@@ -22,9 +28,28 @@ void init() {
 }
 
 void render(uint32_t) {
-    // TODO: improve results display
-    blit::screen.pen = failed ? blit::Pen{255, 0, 0} : blit::Pen{0, 255, 0};
+    blit::screen.pen = {0, 0, 0};
     blit::screen.clear();
+
+    blit::screen.pen = {255, 255, 255};
+    blit::Point p{4, 4};
+    for(auto &group : test_groups) {
+        auto &group_name = std::get<0>(group);
+
+        blit::screen.pen = std::get<1>(group) ? blit::Pen{255, 0, 0} : blit::Pen{0, 255, 0};
+
+        auto text_size = blit::screen.measure_text(group_name, blit::minimal_font);
+
+        if(p.x + text_size.w > blit::screen.bounds.w)
+        {
+            p.x = 4;
+            p.y += 8;
+        }
+
+        blit::screen.text(group_name, blit::minimal_font, p);
+
+        p.x += text_size.w + 4;
+    }
 }
 
 void update(uint32_t) {
